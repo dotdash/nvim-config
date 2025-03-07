2c882305fe20890c44741c84e481bf4f221f8a83
Björn Steinbrink <b.steinbrink@demv.de>
2025-03-07 09:35:42 +0100

    cmp: make <CR> only confirm a completion if it was actively selected

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -51,7 +51,15 @@ return {
               end
             end,
           }),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<CR>'] = cmp.mapping({
+            i = function (fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              else
+                fallback()
+              end
+            end,
+          }),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-e>'] = cmp.mapping.abort(),
